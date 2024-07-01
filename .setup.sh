c64aa714ea8f50b55976a4e6582ce5e8e8b42555
sripwoud <me@sripwoud.xyz>
2024-07-01 14:47:26 +0200

    chore: lint commits with convco with pre-push hook (#3)
    * chore: setup convco as git core.editor
    
    * chore: define pre-push conventional commits lint hook
    
    * refactor: lint with convco only commits that are not in main
    
    * docs: update README

--- .setup.sh
+++ .setup.sh
@@ -31,11 +31,47 @@ maybe_install_local() {
 
 install_dev_deps() {
   log "Installing development dependencies..."
-  crates="dprint cargo-nextest"
+  crates="convco dprint cargo-nextest"
 
   for crate in $crates; do
     maybe_install_local "$crate"
   done
+}
+
+write_pre_push_hook() {
+  cat >.git/hooks/pre-push <<'EOF'
+#!/bin/sh
+alias convco=.cargo/bin/convco
+
+# https://convco.github.io/check/
+z40=0000000000000000000000000000000000000000
+
+while read -r _ local_sha _ remote_sha; do
+  if [ "$local_sha" != $z40 ]; then
+    if [ "$remote_sha" = $z40 ]; then
+      # New branch, examine all commits
+      range="$local_sha"
+    else
+      # Update to existing branch, examine new commits
+      range="$remote_sha..$local_sha"
+    fi
+
+    # Check only the commits that are not in main
+    merge_base=$(git merge-base "$local_sha" main)
+    if [ -n $merge_base ];then
+      range="$merge_base..$local_sha"
+    fi
+
+    # Check for WIP commit
+    if ! convco check "$range"; then
+      exit 1
+    fi
+  fi
+done
+EOF
+
+  chmod +x .git/hooks/pre-push
+  log "\nConventional commits lint hook written to .git/hooks/pre-push"
 }
 
 end_log() {
@@ -45,6 +81,7 @@ end_log() {
 
 main() {
   install_dev_deps
+  write_pre_push_hook
   end_log
 }
 
