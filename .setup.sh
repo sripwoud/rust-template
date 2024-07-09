71bf9ec7cc4aade247d2afd19a9def54d12aaf5b
sripwoud <me@sripwoud.xyz>
2024-07-09 11:04:57 +0200

    refactor: update setup script

--- .setup.sh
+++ .setup.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-set -eou pipefail
+set -eu
 
-CYAN="\033[36m"
 ORANGE="\033[33m"
+RED="\033[31m"
 RESET="\033[0m"
 
 log() {
   printf "%b\n" "$1"
 }
 
-is_bin_locally_available() {
+is_make_available() {
+  if ! command -v make >/dev/null; then
+    log "${RED}error: make is not available.$RESET\nPlease install ${ORANGE}make$RESET for your OS and run this script again."
+    return 1
+  fi
+}
+
+is_crate_bin_locally_available() {
   crate="$1"
   [ -x ".cargo/bin/$crate" ]
 }
@@ -22,10 +29,10 @@ install_local() {
 
 maybe_install_local() {
   crate="$1"
-  if ! is_bin_locally_available "$crate"; then
+  if ! is_crate_bin_locally_available "$crate"; then
     install_local "$crate"
   else
-    log "$ORANGE$crate$RESET already installed locally. Skipping."
+    log "  $ORANGE$crate$RESET already installed locally. Skipping."
   fi
 }
 
@@ -38,50 +45,57 @@ install_dev_deps() {
   done
 }
 
-write_pre_push_hook() {
-  cat >.git/hooks/pre-push <<'EOF'
+write_pre_commit_hook() {
+  echo "make fmt" >.git/hooks/pre-commit
+  chmod +x .git/hooks/pre-commit
+  log "  .git/hooks/pre-commit (formatting)"
+}
+
+write_commit_msg_hook() {
+  cat >.git/hooks/commit-msg <<'EOF'
 #!/bin/sh
 alias convco=.cargo/bin/convco
 
 # https://convco.github.io/check/
 z40=0000000000000000000000000000000000000000
 
-while read -r _ local_sha _ remote_sha; do
-  if [ "$local_sha" != $z40 ]; then
-    if [ "$remote_sha" = $z40 ]; then
-      # New branch, examine all commits
-      range="$local_sha"
-    else
-      # Update to existing branch, examine new commits
-      range="$remote_sha..$local_sha"
-    fi
-
-    # Check only the commits that are not in main
-    merge_base=$(git merge-base "$local_sha" main)
-    if [ -n $merge_base ];then
-      range="$merge_base..$local_sha"
-    fi
-
-    # Check for WIP commit
-    if ! convco check "$range"; then
-      exit 1
-    fi
+main() {
+  if ! cat .git/COMMIT_EDITMSG | convco check --from-stdin --ignore-reverts;then
+    printf "%s\n" "Please refer to https://www.conventionalcommits.org/en/v1.0.0"
+    exit 1
   fi
-done
+}
+
+main
 EOF
 
-  chmod +x .git/hooks/pre-push
-  log "\nConventional commits lint hook written to .git/hooks/pre-push"
+  chmod +x .git/hooks/commit-msg
+  log "  .git/hooks/commit-msg (conventional commits linting)"
+}
+
+write_hooks() {
+  log "Writing hooks..."
+  write_pre_commit_hook
+  write_commit_msg_hook
+}
+
+integrate_convco_with_git() {
+  git config --local core.editor ".cargo/bin/convco commit"
+  # do not use convco for interactive rebase (git rebase -i)
+  git config --local sequence.editor "$EDITOR"
+  log "Integrated convco with git"
 }
 
 end_log() {
-  log "\nTo get started, you can run the make tasks defined in the Makefile.\n"
-  make help | tail -n +2 | head -n -1
+  log "===================\nTo get started, you can run the make tasks defined in the Makefile.\n"
+  make help
 }
 
 main() {
+  is_make_available
   install_dev_deps
-  write_pre_push_hook
+  write_hooks
+  integrate_convco_with_git
   end_log
 }
 
